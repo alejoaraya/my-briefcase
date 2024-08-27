fa835008b8f361dc8a20731eabd3ec7970d8e9a2
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2024-08-27 18:26:22 +0200

    commit: route bug

--- src/BriefcaseApp.tsx
+++ src/BriefcaseApp.tsx
@@ -1,17 +1,21 @@
 import { Route, Routes } from "react-router";
-import { ContentPageLayout } from "./layout";
+import { Footer, Navbar } from "./components";
 import { BriefcasePage, ChatbotPage, ErrorPage } from "./pages";
 import { GalleryHeroesPage } from "./pages/projects/gallery-heroes/GalleryHeroesPage";
 
 export const BriefcaseApp = () => {
   return (
-    <Routes>
-      <Route path='/' element={<ContentPageLayout />}>
+    <>
+      <Navbar />
+      <Routes>
         <Route path='/' element={<BriefcasePage />} />
-        <Route path='danielbot' element={<ChatbotPage />} />
-        <Route path='gallery-heroes' element={<GalleryHeroesPage />} />
+        <Route path='/project/'>
+          <Route path='danielbot' element={<ChatbotPage />} />
+          <Route path='gallery-heroes' element={<GalleryHeroesPage />} />
+        </Route>
         <Route path='/*' element={<ErrorPage />} />
-      </Route>
-    </Routes>
+      </Routes>
+      <Footer />
+    </>
   );
 };
--- src/components/project/components/ProjectsList.tsx
+++ src/components/project/components/ProjectsList.tsx
@@ -4,14 +4,14 @@ export const ProjectsList = () => {
   return (
     <div className='grid grid-flow-row grid-cols-1 gap-10'>
       <ProjectItem
-        url='danielbot'
+        url='project/danielbot'
         description='This project, DanielBot, is an innovative chat-bot that connects to WhatsApp Web to automate various tasks efficiently. It stands out for its low cost and a wide range of customizable options, making it an accessible and versatile solution for businesses looking to enhance their communication through WhatsApp.'
         image='/assets/png/chatbot-background.png'
         title='DanielBot - Chatbot'
         buttonText='CASE WORK'
       />
       <ProjectItem
-        url='gallery-heroes'
+        url='project/gallery-heroes'
         description='This project is a Heroes Gallery that lets users explore and find information about Marvel and DC heroes. Users can search for individual characters or browse the entire collection, providing an engaging experience in the world of superheroes.'
         image='/assets/png/heroes-background.png'
         title='Gallery Heroes - GIFS'
